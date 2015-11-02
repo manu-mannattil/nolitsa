8021876e835e2cbf060fa785d1f5a1ebdb7fc50b
Manu Mannattil <manu.mannattil@gmail.com>
2015-11-03 00:26:02 +0530

    Minor refactoring and optimization.
    We should use NumPy arrays wherever possible.  Appending to a Python
    list will slow things down.

--- nolitsa/data.py
+++ nolitsa/data.py
@@ -14,68 +14,10 @@ from numpy import fft
 from scipy.integrate import odeint
 
 
-def mackey_glass(length=10000, x0=None, a=0.2, b=0.1, c=10.0, tau=23.0,
-                 n=1000, sample=0.46, discard=250):
-    """Generate time series using the Mackey-Glass system.
-
-    Generates time series using the discrete approximation of the
-    Mackey-Glass delay differential equation described in Grassberger &
-    Procaccia (1983).
-
-    Parameters
-    ----------
-    length : int, optional (default = 10000)
-        Length of the time series to be generated.
-    x0 : array, optional (default = random)
-        Initial condition for the discrete map.  Should be of length `n`.
-    a : float, optional (default = 0.2)
-        Constant ``a`` in the Mackey-Glass equation.
-    b : float, optional (default = 0.1)
-        Constant ``b`` in the Mackey-Glass equation.
-    c : float, optional (default = 10.0)
-        Constant ``c`` in the Mackey-Glass equation.
-    tau : float, optional (default = 23.0)
-        Time delay in the Mackey-Glass equation.
-    n : int, optional (default = 1000)
-        The number of discrete steps into which the interval between
-        ``t`` and ``t + tau`` should be divided.  This results in a time
-        step of ``tau/n``.
-    sample : float, optional (default = 0.46)
-        Sampling time of the map.  It is useful to pick something
-        between ``tau/100`` and ``tau/10``, with ``tau/sample`` being a
-        factor of `n`.  This will make sure that there are only whole
-        number indices.
-    discard : int, optional (default = 250)
-        Number of n-steps to discard in order to eliminate transients.
-        A total of ``n*discard`` steps will be discarded.
-
-    Returns
-    -------
-    x : array
-        Array containing the time series.
-    """
-    if not x0:
-        x0 = 0.5 + 0.05 * (-1 + 2 * np.random.random(n))
-
-    sample = int(n * sample / tau)
-    grids = n * discard + sample * length
-
-    x = np.empty(grids)
-    x[:n] = x0
-
-    A = (2 * n - b * tau) / (2 * n + b * tau)
-    B = a * tau / (2 * n + b * tau)
-
-    for i in range(n - 1, grids - 1):
-        x[i + 1] = A * x[i] + B * (x[i - n] / (1 + x[i - n] ** c) +
-                                   x[i - n + 1] / (1 + x[i - n + 1] ** c))
-    return x[n * discard::sample]
-
-
 def henon(length=10000, x0=None, a=1.4, b=0.3, discard=500):
-    """Generate time series from the Henon map.
+    """Generate time series using the Henon map.
 
-    Generates time series from the Henon map.
+    Generates time series using the Henon map.
 
     Parameters
     ----------
@@ -84,26 +26,28 @@ def henon(length=10000, x0=None, a=1.4, b=0.3, discard=500):
     x0 : array, optional (default = random)
         Initial condition for the map.
     a : float, optional (default = 1.4)
-        Constant a in the Henon map.
+        Constant ``a`` in the Henon map.
     b : float, optional (default = 0.3)
-        Constant b in the Henon map.
+        Constant ``b`` in the Henon map.
     discard : int, optional (default = 500)
         Number of steps to discard in order to eliminate transients.
 
     Returns
     -------
-    x : ndarray, shape (length, 2)
+    x : ndarray, shape ``(length, 2)``
         Array containing points in phase space.
     """
+    x = np.empty((length + discard, 2))
+
     if not x0:
-        x = [(0.0, 0.9) + 0.1 * (-1 + 2 * np.random.random(2))]
+        x[0] = (0.0, 0.9) + 0.1 * (-1 + 2 * np.random.random(2))
     else:
-        x = [x0]
+        x[0] = x0
 
-    for i in range(length + discard - 1):
-        x.append([1 - a * x[-1][0] ** 2 + b * x[-1][1], x[-1][0]])
+    for i in range(1, length + discard):
+        x[i] = (1 - a * x[i - 1][0] ** 2 + b * x[i - 1][1], x[i - 1][0])
 
-    return np.asarray(x[discard:])
+    return x[discard:]
 
 
 def ikeda(length=10000, x0=None, alpha=6.0, beta=0.4, gamma=1.0, mu=0.9,
@@ -119,34 +63,35 @@ def ikeda(length=10000, x0=None, alpha=6.0, beta=0.4, gamma=1.0, mu=0.9,
     x0 : array, optional (default = random)
         Initial condition for the map.
     alpha : float, optional (default = 6.0)
-        Constant alpha in the Ikeda map.
+        Constant ``alpha`` in the Ikeda map.
     beta : float, optional (default = 0.4)
-        Constant beta in the Ikeda map.
+        Constant ``beta`` in the Ikeda map.
     gamma : float, optional (default = 1.0)
-        Constant gamma in the Ikeda map.
+        Constant ``gamma`` in the Ikeda map.
     mu : float, optional (default = 0.9)
-        Constant mu in the Ikeda map.
+        Constant ``mu`` in the Ikeda map.
     discard : int, optional (default = 500)
         Number of steps to discard in order to eliminate transients.
 
     Returns
     -------
-    x : ndarray, shape (length, 2)
+    x : ndarray, shape ``(length, 2)``
         Array containing points in phase space.
     """
+    x = np.empty((length + discard, 2))
+
     if not x0:
-        x = [0.1 * (-1 + 2 * np.random.random(2))]
+        x[0] = 0.1 * (-1 + 2 * np.random.random(2))
     else:
-        x = [x0]
+        x[0] = x0
 
-    for i in range(length + discard - 1):
-        phi = beta - alpha / (1 + x[-1][0] ** 2 + x[-1][1] ** 2)
-        x.append([
-            gamma + mu * (x[-1][0] * np.cos(phi) - x[-1][1] * np.sin(phi)),
-            mu * (x[-1][0] * np.sin(phi) + x[-1][1] * np.cos(phi))
-        ])
+    for i in range(1, length + discard):
+        phi = beta - alpha / (1 + x[i - 1][0] ** 2 + x[i - 1][1] ** 2)
+        x[i] = (gamma + mu * (x[i - 1][0] * np.cos(phi) - x[i - 1][1] *
+                np.sin(phi)),
+                mu * (x[i - 1][0] * np.sin(phi) + x[i - 1][1] * np.cos(phi)))
 
-    return np.asarray(x[discard:])
+    return x[discard:]
 
 
 def lorenz(length=10000, x0=None, sigma=10.0, beta=8.0/3.0, rho=28.0,
@@ -193,6 +138,64 @@ def lorenz(length=10000, x0=None, sigma=10.0, beta=8.0/3.0, rho=28.0,
 
     return (t[discard * sample::sample],
             odeint(_lorenz, x0, t)[discard * sample::sample])
+
+
+def mackey_glass(length=10000, x0=None, a=0.2, b=0.1, c=10.0, tau=23.0,
+                 n=1000, sample=0.46, discard=250):
+    """Generate time series using the Mackey-Glass equation.
+
+    Generates time series using the discrete approximation of the
+    Mackey-Glass delay differential equation described in Grassberger &
+    Procaccia (1983).
+
+    Parameters
+    ----------
+    length : int, optional (default = 10000)
+        Length of the time series to be generated.
+    x0 : array, optional (default = random)
+        Initial condition for the discrete map.  Should be of length `n`.
+    a : float, optional (default = 0.2)
+        Constant ``a`` in the Mackey-Glass equation.
+    b : float, optional (default = 0.1)
+        Constant ``b`` in the Mackey-Glass equation.
+    c : float, optional (default = 10.0)
+        Constant ``c`` in the Mackey-Glass equation.
+    tau : float, optional (default = 23.0)
+        Time delay in the Mackey-Glass equation.
+    n : int, optional (default = 1000)
+        The number of discrete steps into which the interval between
+        ``t`` and ``t + tau`` should be divided.  This results in a time
+        step of ``tau/n``.
+    sample : float, optional (default = 0.46)
+        Sampling time of the map.  It is useful to pick something
+        between ``tau/100`` and ``tau/10``, with ``tau/sample`` being a
+        factor of `n`.  This will make sure that there are only whole
+        number indices.
+    discard : int, optional (default = 250)
+        Number of n-steps to discard in order to eliminate transients.
+        A total of ``n*discard`` steps will be discarded.
+
+    Returns
+    -------
+    x : array
+        Array containing the time series.
+    """
+    sample = int(n * sample / tau)
+    grids = n * discard + sample * length
+    x = np.empty(grids)
+
+    if not x0:
+        x[:n] = 0.5 + 0.05 * (-1 + 2 * np.random.random(n))
+    else:
+        x[:n] = x0
+
+    A = (2 * n - b * tau) / (2 * n + b * tau)
+    B = a * tau / (2 * n + b * tau)
+
+    for i in range(n - 1, grids - 1):
+        x[i + 1] = A * x[i] + B * (x[i - n] / (1 + x[i - n] ** c) +
+                                   x[i - n + 1] / (1 + x[i - n + 1] ** c))
+    return x[n * discard::sample]
 
 
 def roessler(length=10000, x0=None, a=0.15, b=0.20, c=10.0, step=0.001,
