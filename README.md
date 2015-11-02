9a9416421dcbbe4f4f824a9364036b633b137241
Manu Mannattil <manu.mannattil@gmail.com>
2015-11-02 23:05:05 +0530

    Added Mackey-Glass delay differential equation.

--- README.md
+++ README.md
@@ -12,10 +12,12 @@ Bibliography
 - Cao, L. (1997). Practical method for determining the minimum embedding dimension of a scalar time series. [_Physica D_, __110__, 43](https://dx.doi.org/10.1016/S0167-2789(97)00118-8).
 - Ehlers, C.L., Havstad, J., Prichard, D. & Theiler, J. (1998). Low doses of ethanol reduce evidence for nonlinear structure in brain activity. [_J. Neurosci._  __18__, 7474](http://www.jneurosci.org/content/18/18/7474).
 - Fraser, A.M. & Swinney, H.L. (1986). Independent coordinates for strange attractors from mutual information. [_Phys. Rev. A_, __33__, 1134](https://dx.doi.org/10.1103/PhysRevA.33.1134).
+- Grassberger, P. & Procaccia, I. (1983). Measuring the strangeness of strange attractors. [_Physica D_, __9__, 189](https://dx.doi.org/10.1016/0167-2789(83)90298-1).
 - Kennel, M.B., Brown, R. & Abarbanel, H.D.I. (1992). Determining embedding dimension for phase-space reconstruction using a geometrical construction. [_Phys. Rev. A_, __45__, 3403](https://dx.doi.org/10.1103/PhysRevA.45.3403).
 - Schreiber, T. & Schmitz, A. (1996). Improved surrogate data for nonlinearity tests. [_Phys. Rev. Lett._, __77__, 635](https://dx.doi.org/10.1103/PhysRevLett.77.635).
 - Sprott, J.C. (2003). _Chaos and Time-Series Analysis_, Oxford University Press, New York.
 - Theiler, J., Eubank, S., Longtin, A., Galdrikian, B. & Farmer, J.D. (1992). Testing for nonlinearity in time series. [_Physica D_, __58__, 77](https://dx.doi.org/10.1016/0167-2789(92)90102-S).
+
 
 
 License
--- nolitsa/data.py
+++ nolitsa/data.py
@@ -2,8 +2,7 @@
 
 """Functions to generate time series of some popular chaotic systems.
 
-The parameters, initial conditions, etc. have been taken from Appendix A
-of Sprott (2003).
+Parameters and initial conditions have been taken from Sprott (2003).
 """
 
 
@@ -13,6 +12,64 @@ import numpy as np
 
 from numpy import fft
 from scipy.integrate import odeint
+
+
+def mackey_glass(length=10000, x0=None, a=0.2, b=0.1, c=10.0, tau=23.0,
+                 n=1000, sample=0.46, discard=250):
+    """Generate time series using the Mackey-Glass system.
+
+    Generates time series using the discrete approximation of the
+    Mackey-Glass delay differential equation described in Grassberger &
+    Procaccia (1983).
+
+    Parameters
+    ----------
+    length : int, optional (default = 10000)
+        Length of the time series to be generated.
+    x0 : array, optional (default = random)
+        Initial condition for the discrete map.  Should be of length `n`.
+    a : float, optional (default = 0.2)
+        Constant ``a`` in the Mackey-Glass equation.
+    b : float, optional (default = 0.1)
+        Constant ``b`` in the Mackey-Glass equation.
+    c : float, optional (default = 10.0)
+        Constant ``c`` in the Mackey-Glass equation.
+    tau : float, optional (default = 23.0)
+        Time delay in the Mackey-Glass equation.
+    n : int, optional (default = 1000)
+        The number of discrete steps into which the interval between
+        ``t`` and ``t + tau`` should be divided.  This results in a time
+        step of ``tau/n``.
+    sample : float, optional (default = 0.46)
+        Sampling time of the map.  It is useful to pick something
+        between ``tau/100`` and ``tau/10``, with ``tau/sample`` being a
+        factor of `n`.  This will make sure that there are only whole
+        number indices.
+    discard : int, optional (default = 250)
+        Number of n-steps to discard in order to eliminate transients.
+        A total of ``n*discard`` steps will be discarded.
+
+    Returns
+    -------
+    x : array
+        Array containing the time series.
+    """
+    if not x0:
+        x0 = 0.5 + 0.05 * (-1 + 2 * np.random.random(n))
+
+    sample = int(n * sample / tau)
+    grids = n * discard + sample * length
+
+    x = np.empty(grids)
+    x[:n] = x0
+
+    A = (2 * n - b * tau) / (2 * n + b * tau)
+    B = a * tau / (2 * n + b * tau)
+
+    for i in range(n - 1, grids - 1):
+        x[i + 1] = A * x[i] + B * (x[i - n] / (1 + x[i - n] ** c) +
+                                   x[i - n + 1] / (1 + x[i - n + 1] ** c))
+    return x[n * discard::sample]
 
 
 def henon(length=10000, x0=None, a=1.4, b=0.3, discard=500):
